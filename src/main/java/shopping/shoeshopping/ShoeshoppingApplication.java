54f37f754fb5ffcf4d128091081e993ac40f76e2
Hyeonseong Lee <zzzdlgustjd@gmail.com>
2021-05-09 02:28:22 +0900

    thymeleaf 추가

---
+++ src/main/java/shopping/shoeshopping/ShoeshoppingApplication.java
@@ -0,0 +1,13 @@
+package shopping.shoeshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoeshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shopping/shoeshopping/ShoeshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package shopping.shoeshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
